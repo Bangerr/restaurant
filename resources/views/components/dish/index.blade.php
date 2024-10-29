c6a8755df1647a114defb3c01e36942686e55c5b
Bangerr <devilism09@gmail.com>
2024-10-29 23:08:23 +0100

    fixed card's width

--- resources/views/components/dish/index.blade.php
+++ resources/views/components/dish/index.blade.php
@@ -1,10 +1,11 @@
-<div class="border-2 w-[200px]">
-    <h2>{{ $dish->title() }}</h2>
+<div class="flex flex-col gap-3 p-2">
+    <h2 class="text-xl inline-flex">
+        <p class="">Dish name: </p>{{ $dish->title() }}
+    </h2>
     <div class="dish-card">
-        <h2>{{ $dish->title }}</h2>
         <p>{{ $dish->description }}</p>
         <img src="{{ $dish->image_path }}" alt="{{ $dish->title }}">
-        <p>{{ $dish->instructions }}</p>
+        <p class="w-[65ch] whitespace-nowrap truncate overflow-hidden">{{ $dish->instructions }}</p>
     </div>
 
 </div>
--- resources/views/pages/dishes/index.blade.php
+++ resources/views/pages/dishes/index.blade.php
@@ -4,19 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Electrolize&family=Kode+Mono:wght@400..700&family=Slackside+One&display=swap"
+        rel="stylesheet">
+    <script src="https://kit.fontawesome.com/44b694a7ec.js" crossorigin="anonymous"></script>
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    @endif
 </head>
 
-<body class="w-3/4 mx-auto">
+<body class="w-3/4 mx-auto h-full">
     <x-navbar />
 
-    <div class="w-1/2 mx-auto">
+    <div class="w-full mx-auto mt-5">
 
-        <div class="grid grid-cols-2">
+        <div class="flex flex-row gap-x-5 ">
 
             @foreach ($dishes as $dish)
-                <div class="border w-[200px]">
+                <div class="flex border w-[200px] justify-start ">
                     <x-dish.index :dish="$dish" />
                 </div>
             @endforeach
