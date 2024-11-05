d3027f22e701588c891bc6a18f10e9cbc39bc680
Bangerr <devilism09@gmail.com>
2024-11-05 22:22:32 +0100

    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="px-5 flex items-center justify-between">
+<footer class="flex items-center justify-between">
     <div class="flex" aria-label="footer">
         <a class="text-[1.3rem] font-semibold hover:underline decoration-wavy "
             href="https://www.google.com/maps/place/77%C2%B011'42.8%22N+44%C2%B055'15.1%22W/@77.1952302,-44.9234419,17z/data=!3m1!4b1!4m4!3m3!8m2!3d77.195229!4d-44.920867?entry=ttu"
