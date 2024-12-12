afd7ece74973164670ec25ced6dc7bba9d27585e
Bangerr <devilism09@gmail.com>
2024-12-13 00:46:38 +0100

    integrated store Dish with image on neon/aws s3(for images)

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -10,11 +10,70 @@ class DishController extends Controller
 
     public function index()
     {
-
         return view('pages.dishes.index', [
             'dishes' => Dish::all()
         ]);
     }
+
+    // public function store(Request $request)
+    // {
+
+    //     try {
+    //         $request->validate([
+    //             'title' => 'required',
+    //             'description' => 'required',
+    //             'instructions' => 'required',
+    //             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    //         ]);
+
+
+    //         if ($request->hasFile('image')) {
+    //             //Get the file extension
+    //             $extension  = request()->file('image')->getClientOriginalExtension();
+
+    //             //Generate a unique name for the image
+    //             $image_name = time() . '_' . $request->title . '.' . $extension;
+
+    //             //Store the image in the S3
+    //             $path = $request->file('image')->storeAs(
+    //                 'images',
+    //                 $image_name,
+    //                 's3'
+    //             );
+
+    //             // Get the full URL of the image
+    //             $url = Storage::disk('s3')->url($path);
+    //             \Log::info('Image URL: ' . $url);
+    //             echo $url;
+
+
+
+    //             //Save the record in the database
+    //             Image::create([
+    //                 'title' => $request->title,
+    //                 'image' => $path
+    //             ]);
+
+    //             $dish = new Dish();
+    //             $dish->title = $request->title;
+    //             $dish->description = $request->description;
+    //             $dish->instructions = $request->instructions;
+    //             $dish->image_path = $url;
+    //             $dish->create();
+    //             $dish->save();
+
+
+    //             return redirect()->back()->with([
+    //                 'message' => "Image uploaded successfully",
+    //             ]);
+    //         }
+    //     } catch (\Exception $e) {
+    //         return redirect()->back()->with([
+    //             'error' => "Upload failed: " . $e->getMessage()
+    //         ]);
+    //     }
+    // }
+
     /*
     * Show the Dynamic DishId
     * @params $dishId
--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Dish;
 
 class ImageController extends Controller
 {
@@ -19,33 +20,49 @@ class ImageController extends Controller
     public function store(Request $request)
     {
 
-        // try {
-        //     Storage::disk('s3')->put('test.txt', 'Hello World');
-        //     echo "Connection successful";
-        // } catch (\Exception $e) {
-        //     echo "Connection failed: " . $e->getMessage();
-        // }
-        // $path = $request->file('image')->store('images', 's3');
-
-        // return $path;
-
         try {
             $request->validate([
                 'title' => 'required',
+                'description' => 'required',
+                'instructions' => 'required',
                 'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             ]);
+
+
             if ($request->hasFile('image')) {
-                $extension  = request()->file('image')->getClientOriginalExtension(); //This is to get the extension of the image file just uploaded
+                //Get the file extension
+                $extension  = request()->file('image')->getClientOriginalExtension();
+
+                //Generate a unique name for the image
                 $image_name = time() . '_' . $request->title . '.' . $extension;
+
+                //Store the image in the S3
                 $path = $request->file('image')->storeAs(
                     'images',
                     $image_name,
                     's3'
                 );
+
+                // Get the full URL of the image
+                $url = Storage::disk('s3')->url($path);
+                \Log::info('Image URL: ' . $url);
+                echo $url;
+
+
+
+                //Save the record in the database
                 Image::create([
                     'title' => $request->title,
-                    'image' => $path
+                    'image' => $url
                 ]);
+
+                Dish::create([
+                    'title' => $request->title,
+                    'description' => $request->description,
+                    'instructions' => $request->instructions,
+                    'image_path' => $url,
+                ]);
+
                 return redirect()->back()->with([
                     'message' => "Image uploaded successfully",
                 ]);
